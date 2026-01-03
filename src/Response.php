03fa89a12866e412cd673743305feadf5eab0740
Jahidul Pabel Islam <jahidul@d3r.com>
2026-01-03 13:54:55 +0000

    Allow JsonSerializable for Json response body

--- src/Response.php
+++ src/Response.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace JPI\HTTP;
 
+use JsonSerializable;
+
 /**
  * Represents an HTTP response.
  *
@@ -30,7 +32,7 @@ class Response extends Message {
      */
     public static function json(
         int $statusCode = 500,
-        array $body = [],
+        JsonSerializable|array $body = [],
         array $headers = [],
         float $protocolVersion = 1.1
     ): Response {
@@ -88,7 +90,7 @@ class Response extends Message {
         return $this->statusMessage;
     }
 
-    public function withJSON(array $body): Response {
+    public function withJSON(JsonSerializable|array $body): Response {
         $this->body = json_encode($body);
         $this->setHeader("Content-Type", "application/json");
         return $this;
